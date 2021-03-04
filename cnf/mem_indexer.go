d18caec2f5e0191789d51d63598dbc7e6cd6eea0
Yi Wang <yi.wang@rtl.nl>
2021-03-04 17:30:58 +0100

    build inverted list step 2:

--- cnf/mem_indexer.go
+++ cnf/mem_indexer.go
@@ -14,14 +14,36 @@ func (k Key) hash() string {
 }
 
 type memoryIndexer struct {
-	s map[string][]set.SortedSet
+	sortedSetMap map[string][]set.SortedSet
+	pListsMap    map[string]PostingLists
+}
+
+func (m *memoryIndexer) buildPostingList(s set.SortedSet) PostingList {
+	sl := s.ToSlice()
+	p := make(PostingList, len(sl))
+	for i, item := range sl {
+		p[i] = item.(*PostingEntry)
+	}
+
+	return p
+}
+
+func (m *memoryIndexer) Build() {
+	m.pListsMap = make(map[string]PostingLists, len(m.sortedSetMap))
+	for key, sets := range m.sortedSetMap {
+		p := make(PostingLists, len(sets))
+		for i, set := range sets {
+			p[i] = m.buildPostingList(set)
+		}
+		m.pListsMap[key] = p
+	}
 }
 
 func (m *memoryIndexer) add(k string, entry *PostingEntry) {
-	sortSets, exists := m.s[k]
+	sortSets, exists := m.sortedSetMap[k]
 	if !exists || sortSets == nil {
 		sortSets = append(sortSets, set.NewSortedSet())
-		m.s[k] = sortSets
+		m.sortedSetMap[k] = sortSets
 	}
 
 	inserted := false
@@ -33,7 +55,7 @@ func (m *memoryIndexer) add(k string, entry *PostingEntry) {
 	}
 
 	if !inserted {
-		m.s[k] = append(sortSets, set.NewSortedSet())
+		m.sortedSetMap[k] = append(sortSets, set.NewSortedSet())
 		m.add(k, entry)
 	}
 }
--- cnf/mem_indexer_test.go
+++ cnf/mem_indexer_test.go
@@ -9,7 +9,7 @@ import (
 
 func Test_memoryIndexer_add(t *testing.T) {
 	m := &memoryIndexer{
-		s: make(map[string][]set.SortedSet),
+		sortedSetMap: make(map[string][]set.SortedSet),
 	}
 
 	m.add("A:1", &PostingEntry{ID: int64(1), DisjunctionID: 0})
@@ -19,14 +19,49 @@ func Test_memoryIndexer_add(t *testing.T) {
 	m.add("A:1", &PostingEntry{ID: int64(3), DisjunctionID: 1})
 	m.add("B:1", &PostingEntry{ID: int64(1), DisjunctionID: 0})
 
-	a1 := m.s["A:1"][0].ToSlice()
+	a1 := m.sortedSetMap["A:1"][0].ToSlice()
 	for i, s := range a1 {
 		assert.Equal(t, int64(i+1), s.SortID())
 	}
 
-	a2 := m.s["A:1"][1].ToSlice()
+	a2 := m.sortedSetMap["A:1"][1].ToSlice()
 	assert.Equal(t, int64(3), a2[0].SortID())
 
-	b1 := m.s["B:1"][0].ToSlice()
+	b1 := m.sortedSetMap["B:1"][0].ToSlice()
 	assert.Equal(t, int64(1), b1[0].SortID())
+}
+
+func Test_memoryIndexer_Build(t *testing.T) {
+	s1 := set.NewSortedSet()
+	s1.Add(&PostingEntry{ID: int64(1), DisjunctionID: 0})
+	s1.Add(&PostingEntry{ID: int64(2), DisjunctionID: 0})
+	s1.Add(&PostingEntry{ID: int64(3), DisjunctionID: 0})
+
+	s2 := set.NewSortedSet()
+	s2.Add(&PostingEntry{ID: int64(3), DisjunctionID: 1})
+
+	s3 := set.NewSortedSet()
+	s3.Add(&PostingEntry{ID: int64(1), DisjunctionID: 0})
+
+	m := &memoryIndexer{
+		sortedSetMap: map[string][]set.SortedSet{
+			"A:1": {
+				s1, s2,
+			},
+			"B:1": {
+				s3,
+			},
+		},
+	}
+
+	m.Build()
+
+	plA := m.pListsMap["A:1"]
+	assert.Equal(t, 2, len(plA))
+	assert.Equal(t, 3, len(plA[0]))
+	assert.Equal(t, 1, len(plA[1]))
+
+	plB := m.pListsMap["B:1"]
+	assert.Equal(t, 1, len(plB))
+	assert.Equal(t, 1, len(plB[0]))
 }
