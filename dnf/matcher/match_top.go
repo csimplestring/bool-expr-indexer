797a53972befdb0227c62ffd467eb52110ffd445
Yi Wang <yi.wang@rtl.nl>
2021-03-24 17:21:09 +0100

    support top N matching

--- dnf/matcher/match_top.go
+++ dnf/matcher/match_top.go
@@ -1,81 +1,12 @@
 package matcher
 
 import (
-	"sort"
-
 	"github.com/csimplestring/bool-expr-indexer/dnf/expr"
 	"github.com/csimplestring/bool-expr-indexer/dnf/indexer"
 	"github.com/csimplestring/bool-expr-indexer/dnf/indexer/posting"
 	"github.com/csimplestring/bool-expr-indexer/dnf/matcher/pq"
 	"github.com/csimplestring/bool-expr-indexer/dnf/scorer"
 )
-
-type ranker struct {
-	assignment    expr.Assignment
-	sWeightLookup map[string]int
-}
-
-func newRanker(assignment expr.Assignment) *ranker {
-	u := &ranker{
-		assignment: assignment,
-	}
-
-	sWeightLookup := make(map[string]int)
-	for _, label := range assignment {
-		sWeightLookup[u.formatKey(label.Name, label.Value)] = label.Weight
-	}
-	u.sWeightLookup = sWeightLookup
-
-	return u
-}
-
-func (u *ranker) formatKey(name, value string) string {
-	return name + ":" + value
-}
-
-// lists must be sorted
-func (u *ranker) conjunctionUB(K int, lists postingLists) int {
-
-	score := 0
-	for i := 0; i <= K && i < lists.Len(); i++ {
-		name := lists[i].name
-		value := lists[i].value
-		if w, exists := u.sWeightLookup[u.formatKey(name, value)]; exists {
-			score += w * lists[i].ub
-		}
-	}
-	return score
-}
-
-func (u *ranker) listUB(topN int, lists postingLists) int {
-
-	scores := make([]int, lists.Len())
-	for i, list := range lists {
-		if w, exists := u.sWeightLookup[u.formatKey(list.name, list.value)]; exists {
-			scores[i] = w * list.ub
-		} else {
-			scores[i] = 0
-		}
-	}
-	// get topN
-	sort.Ints(scores)
-	totalUB := 0
-	for i := 0; i < topN && i < len(scores); i++ {
-		totalUB += scores[i]
-	}
-
-	return totalUB
-}
-
-func extractIDs(q pq.MinMaxPriorityQueue) []int {
-	ids := make([]int, q.Len())
-	i := 0
-	for q.Len() != 0 {
-		ids[i] = q.PopMin().(*pq.IntItem).Value().(int)
-		i++
-	}
-	return ids
-}
 
 type TopNMatcher interface {
 	MatchTopN(topN int, indexer indexer.Indexer, assignment expr.Assignment) []int
@@ -92,7 +23,6 @@ type topNMatcher struct {
 }
 
 func (m *topNMatcher) MatchTopN(topN int, indexer indexer.Indexer, assignment expr.Assignment) []int {
-	results := make([]int, 0, 1024)
 	ubCalculator := newRanker(assignment)
 	q := pq.New(topN)
 
@@ -109,14 +39,14 @@ func (m *topNMatcher) MatchTopN(topN int, indexer indexer.Indexer, assignment ex
 			continue
 		}
 
-		listUB := ubCalculator.listUB(K, pLists)
+		listUB := ubCalculator.calculateListUB(K, pLists)
 		if q.Len() == topN && q.PeekMin().Priority() > listUB {
 			continue
 		}
 
 		pLists.sortByCurrent()
 		for pLists[K-1].current() != posting.EOL {
-			conjunctionUB := ubCalculator.conjunctionUB(K-1, pLists)
+			conjunctionUB := ubCalculator.calculateEntryUB(K-1, pLists)
 
 			if q.Len() == topN && q.PeekMin().Priority() > conjunctionUB {
 				nextID := pLists[K-1].current().CID() + 1
@@ -143,7 +73,6 @@ func (m *topNMatcher) MatchTopN(topN int, indexer indexer.Indexer, assignment ex
 
 				} else {
 					q.Push(&pq.IntItem{Val: int(pLists[K-1].current().CID()), Prior: conjunctionUB})
-					results = append(results, int(pLists[K-1].current().CID()))
 				}
 
 				nextID = pLists[K-1].current().CID() + 1
@@ -161,4 +90,14 @@ func (m *topNMatcher) MatchTopN(topN int, indexer indexer.Indexer, assignment ex
 	}
 
 	return extractIDs(q)
+}
+
+func extractIDs(q pq.MinMaxPriorityQueue) []int {
+	ids := make([]int, q.Len())
+	i := 0
+	for q.Len() != 0 {
+		ids[i] = q.PopMin().(*pq.IntItem).Value().(int)
+		i++
+	}
+	return ids
 }
---
+++ dnf/matcher/ranker.go
@@ -0,0 +1,64 @@
+package matcher
+
+import (
+	"sort"
+
+	"github.com/csimplestring/bool-expr-indexer/dnf/expr"
+)
+
+type ranker struct {
+	assignment    expr.Assignment
+	sWeightLookup map[string]int
+}
+
+func newRanker(assignment expr.Assignment) *ranker {
+	u := &ranker{
+		assignment: assignment,
+	}
+
+	sWeightLookup := make(map[string]int)
+	for _, label := range assignment {
+		sWeightLookup[u.formatKey(label.Name, label.Value)] = label.Weight
+	}
+	u.sWeightLookup = sWeightLookup
+
+	return u
+}
+
+func (u *ranker) formatKey(name, value string) string {
+	return name + ":" + value
+}
+
+// lists must be sorted
+func (u *ranker) calculateEntryUB(K int, lists postingLists) int {
+
+	score := 0
+	for i := 0; i <= K && i < lists.Len(); i++ {
+		name := lists[i].name
+		value := lists[i].value
+		if w, exists := u.sWeightLookup[u.formatKey(name, value)]; exists {
+			score += w * lists[i].ub
+		}
+	}
+	return score
+}
+
+func (u *ranker) calculateListUB(topN int, lists postingLists) int {
+
+	scores := make([]int, lists.Len())
+	for i, list := range lists {
+		if w, exists := u.sWeightLookup[u.formatKey(list.name, list.value)]; exists {
+			scores[i] = w * list.ub
+		} else {
+			scores[i] = 0
+		}
+	}
+	// get topN
+	sort.Ints(scores)
+	totalUB := 0
+	for i := 0; i < topN && i < len(scores); i++ {
+		totalUB += scores[i]
+	}
+
+	return totalUB
+}
